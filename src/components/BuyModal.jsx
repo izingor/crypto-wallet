bc88eccc2b55dffcbb69124155d7c07c66a8642f
Igor Ziskin <izingor@gmail.com>
2022-06-07 22:32:13 +0300

    changel buy modal ui

--- src/components/BuyModal.jsx
+++ src/components/BuyModal.jsx
@@ -1,52 +1,52 @@
-import { CloseXBtn } from './buttons/CloseXBtn';
-import { SmallBtn } from './buttons/SmallBtn';
-import { useDispatch, useSelector } from 'react-redux';
-import { uiState, onBuyModalChanged } from '../store/modules/ui.store';
-import { user } from '../store/modules/user.store';
-import { CurrencyInputs } from './inputs/CurrencyInputs';
+import { CloseXBtn } from './buttons/CloseXBtn'
+import { SmallBtn } from './buttons/SmallBtn'
+import { useDispatch, useSelector } from 'react-redux'
+import { uiState, onBuyModalChanged } from '../store/modules/ui.store'
+import { user } from '../store/modules/user.store'
+import { CurrencyInputs } from './inputs/CurrencyInputs'
 
 export const BuyModal = () => {
-	const { isBuyModaLOpen, clickedCoin } = useSelector(uiState);
-	const activeUser = useSelector(user);
-	const dispatch = useDispatch();
+	const { isBuyModaLOpen, clickedCoin } = useSelector(uiState)
+	const activeUser = useSelector(user)
+	const dispatch = useDispatch()
 	const closeModal = () => {
-		dispatch(onBuyModalChanged());
+		dispatch(onBuyModalChanged())
 		// console.log('closing modal');
-	};
+	}
 
 	const buyCoins = () => {
-		console.log('buying coins');
-	};
+		console.log('buying coins')
+	}
 
 	return (
 		<div
-			id="medium-modal"
-			className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full  h-modal h-full"
+			id='medium-modal'
+			className='overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full  h-modal h-full'
 			hidden={!isBuyModaLOpen}
 		>
 			{clickedCoin && (
-				<div className="relative h-full w-full">
-					<div className="relative  w-9/12  inset-y-auto right-1/4 t md:w-2/4 md:inset-x-auto bg-white rounded-lg shadow translate-x-1/2 translate-y-1/4">
-						<div className="flex justify-between items-center p-5 rounded-t border-b ">
-							<h3 className="text-xl font-medium text-gray-900 ">
-								Purchase {clickedCoin.name}
+				<div className='relative h-full w-full'>
+					<div className='relative  w-9/12  inset-y-auto right-1/4 t md:w-2/4 md:inset-x-auto bg-white rounded-lg shadow translate-x-1/2 translate-y-1/4'>
+						<div className='flex justify-between items-center p-5 rounded-t border-b '>
+							<img src={clickedCoin.iconUrl} className=' mr-4 w-14 h-14' alt='' />
+							<h3 className='text-xl font-medium text-gray-900 '>
+								1 {clickedCoin.name} = ${clickedCoin.price}
 							</h3>
 							<CloseXBtn handleClick={closeModal} />
 						</div>
-						<div className="p-6 space-y-6">
-							<img src={clickedCoin.iconUrl} className="w-14 h-14" alt="" />
-							<p className="text-base leading-relaxed text-gray-500 ">
+						<div className='p-6 space-y-6'>
+							<p className='text-base leading-relaxed text-gray-500 '>
 								<strong>Your Balance </strong>- ${activeUser.usdBalance}
 							</p>
 							<CurrencyInputs />
 						</div>
-						<div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200">
-							<SmallBtn txt="Buy" type="buy" handleClick={buyCoins} />
-							<SmallBtn txt="Back" type="back" handleClick={closeModal} />
+						<div className='flex items-center p-6 space-x-2 rounded-b border-t border-gray-200'>
+							<SmallBtn txt='Buy' type='buy' handleClick={buyCoins} />
+							<SmallBtn txt='Back' type='back' handleClick={closeModal} />
 						</div>
 					</div>
 				</div>
 			)}
 		</div>
-	);
-};
+	)
+}
--- src/components/inputs/CurrencyInputs.jsx
+++ src/components/inputs/CurrencyInputs.jsx
@@ -11,13 +11,7 @@ export const CurrencyInputs = () => {
 				required
 			/>
 			<img src={arrow} className = 'w-14 h-14'alt='' />
-			<input
-				type='search'
-				id='default-search'
-				class='block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 '
-				placeholder='USD'
-				required
-			/>
+		
 		</div>
 	)
 }
